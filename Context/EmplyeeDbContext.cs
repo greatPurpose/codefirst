3a182ed87a09ff79ecce31475a17d4f4b364e3ae
greatPurpose <great.purpose@outlook.com>
2019-11-22 04:05:37 +0100

    test codefirst EntityFramework

---
+++ Context/EmplyeeDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFristEF.Context
+{
+    public class EmplyeeDbContext : DbContext
+    {
+        public EmplyeeDbContext(DbContextOptions option) :  base(option)
+        {
+
+        }
+
+        DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Migrations/20191122024856_addedsalary.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodeFristEF.Migrations
+{
+    public partial class addedsalary : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Salary",
+                table: "Employee",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Salary",
+                table: "Employees");
+        }
+    }
+}
